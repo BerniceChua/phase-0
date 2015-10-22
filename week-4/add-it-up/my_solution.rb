04d09f8ac3d9847191896c75ccee18d043f1c18a
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-22 11:38:21 -0700

    Create directory 'add-it-up' & 2 .rb files.

---
+++ week-4/add-it-up/my_solution.rb
@@ -0,0 +1,46 @@
+# Add it up!
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# I worked on this challenge [with: Shea Munion].
+
+# 0. total Pseudocode
+# make sure all pseudocode is commented out!
+
+# Input: array of numbers
+# Output: sum from the array of numbers
+# Steps to solve the problem.
+
+=begin
+Step 1: get user input.
+Step 2: make sure inputs are numbers.
+Step 3: make a variable to hold results -- "sum".
+Step 4: Until end of the array is reached: get 1 number 
+in the array & add it to next one, and the result gets stored.
+Step 5: return the sum.
+=end
+
+# 1. total initial solution
+
+
+
+# 3. total refactored solution
+
+
+
+# 4. sentence_maker pseudocode
+# make sure all pseudocode is commented out!
+# Input:
+# Output:
+# Steps to solve the problem.
+
+
+# 5. sentence_maker initial solution
+
+
+
+# 6. sentence_maker refactored solution
+
+
