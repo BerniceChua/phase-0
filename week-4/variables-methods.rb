dbbc86a89f05497aefd84b2fb320d0d3324b715f
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-21 17:49:32 -0700

    Create variables-methodes.rb.

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,12 @@
+=begin
+Full name greeting: Write a program that asks for a person's first 
+name, then middle, then last. Finally, it should greet the person 
+using their full name.
+=end
+
+=begin
+Bigger, better favorite number: Write a program that asks for a 
+person's favorite number. Have your program add 1 to the number, 
+and then suggest the result as a bigger and better number.  (Do be 
+tactful about it, though.)
+=end
