ff6118e3d65c0b67984fb7e90497edd82988daa2
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-22 00:29:33 -0700

    Create 'math' directory, & the 2 files inside it.

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,22 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ #your code here
+end
+
+def subtract(num_1, num_2)
+ #your code here
+end
+
+def multiply(num_1, num_2)
+ #your code here
+end
+
+def divide(num_1, num_2)
+ #your code here
+end
