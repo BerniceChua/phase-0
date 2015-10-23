64c77b960c41d273eaf923aff4a9aa8b438cc2d4
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-22 23:27:39 -0700

    Complete answers to 4.6.3 Shortest String in Array in my_solution.rb.

--- week-4/shortest-string/my_solution.rb
+++ week-4/shortest-string/my_solution.rb
@@ -13,20 +13,35 @@
 =begin
 Pseudocode
     Input = array of integers
-    Output = largest integer
+    Output = shortest integer
 
-    Step 1: get user input (array of integers, called "list_of_nums").
-    Step 2: make a variable to hold results -- "largest".
-    Step 3: set largest equal to first integer of "list_of_nums"
-    Step 4: UNTIL end of the array is reached: compare "largest" to next 
-        number in the array.  
-    Step 4.1: IF the next number is larger than the number in "largest", 
-        replace "largest" with the next number.
-    Step 4: return the result of largest.
+    Step 1: get user input (array of Strings, called "list_of_words").
+    Step 2: make a variable to hold results -- "shortest".
+    Step 3: set "shortest" equal to first element of "list_of_words"
+    Step 4: UNTIL end of the array is reached: compare "shortest" to next 
+        word in the array.  
+    Step 4.1: IF the next word is shorter than the word in "shortest", 
+        replace "shortest" with the next word.
+    Step 4: return the result of shortest.
 =end
 
 
 #Your Solution Below
+# initial solution
 def shortest_string(list_of_words)
   # Your code goes here!
+  shortest = list_of_words[0]
+
+  list_of_words.each { |word| 
+    if word.length < shortest.length
+        shortest = word
+    end
+  }
+
+  return shortest
+end
+
+# refactored solution
+def shortest_string(list_of_words)
+  list_of_words.min_by { |word| word.length } # trying out implicit returns
 end
