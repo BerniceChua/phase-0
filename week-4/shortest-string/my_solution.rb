9f2ae6b128c15c9f8c307dfb0d4d220fbe776cdf
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-22 23:05:01 -0700

    Add shortest-string directory & 2 .rb files.

---
+++ week-4/shortest-string/my_solution.rb
@@ -0,0 +1,32 @@
+# Shortest String
+
+# I worked on this challenge [by myself; because this is a solo challenge].
+
+# shortest_string is a method that takes an array of strings as its input
+# and returns the shortest string
+#
+# +list_of_words+ is an array of strings
+# shortest_string(array) should return the shortest string in the +list_of_words+
+#
+# If +list_of_words+ is empty the method should return nil
+
+=begin
+Pseudocode
+    Input = array of integers
+    Output = largest integer
+
+    Step 1: get user input (array of integers, called "list_of_nums").
+    Step 2: make a variable to hold results -- "largest".
+    Step 3: set largest equal to first integer of "list_of_nums"
+    Step 4: UNTIL end of the array is reached: compare "largest" to next 
+        number in the array.  
+    Step 4.1: IF the next number is larger than the number in "largest", 
+        replace "largest" with the next number.
+    Step 4: return the result of largest.
+=end
+
+
+#Your Solution Below
+def shortest_string(list_of_words)
+  # Your code goes here!
+end
