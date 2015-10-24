82a09b590ba812d0b6419abf1ab3bd69a8c3247f
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-23 23:30:30 -0700

    Add goog-guess directory & its 2 .rb files.

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,30 @@
+# Good Guess
+
+# I worked on this challenge [by myself, because the description said "(not as good for pairing)"].
+
+=begin
+Inputs = an integer value (I'll call it 'number_answer')
+Output = a boolean (true if answer matches 42, false if not)
+
+Step 1: get input
+Step 2: check if answer matches 42
+  Step 2.1: IF number_answer is the same as 42, then program returns true.
+  Step 2.2: return false otherwise.
+Step 3: return output
+=end
+
+# Your Solution Below
+
+# initial solution
+def good_guess?(number_answer)
+    if number_answer == 42
+        return true
+    else
+        return false
+    end
+end
+
+# refactored solution
+# def good_guess?(number_answer)
+#     true if number_answer == 42 else false
+# end
