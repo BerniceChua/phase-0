047e5a2ab673b179ef94e107b93eec25a1c3e762
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-29 11:32:35 -0700

    Add initial pseudocode.

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -13,9 +13,28 @@
 
 # 0. Pseudocode
 
+=begin
 # What is the input?
+INPUT = array
+
 # What is the output? (i.e. What should the code return?)
+output for pad = a different modified array, 
+    but original array is unchanged
+output for pad! = the input array, but modified
+
 # What are the steps needed to solve the problem?
+
+for pad
+Step 1: get inputs:
+    1 array
+    1 integer (total # of elements in array)
+    OPTIONAL: 1 argument for what we will use to fill out array
+        (if not specified, default = nil [the actual value of nil, not a String that says "nil"])
+Step 2:
+
+
+
+=end
 
 
 # 1. Initial Solution
