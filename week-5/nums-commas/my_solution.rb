78da89decbca0d9d4a9e8da102d7e5049466ae8b
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-29 03:30:14 -0700

    Add reflection to nums-commas/my_solution.rb.

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -53,7 +53,7 @@ Step 4: return the commaized version of input.
 # 1. Initial Solution
 
 def separate_comma(number)
-    reversed_array = number.to_s.split("").reverse
+    reversed_array = number.to_i.to_s.split("").reverse
     commaized = []
 
     current_digit = 0
@@ -74,7 +74,7 @@ end
 # Alternate
 
 def separate_comma(number)
-    number_array = number.to_s.split("")
+    number_array = number.to_i.to_s.split("")
     commaized = []
 
     current_digit = number_array.length - 1
@@ -97,7 +97,7 @@ end
 # 2. Refactored Solution
 
 def separate_comma(number)
-    number_array = number.to_s.split("")
+    number_array = number.to_i.to_s.split("")
     commaized = []
 
     while number_array.length != 0
@@ -108,7 +108,7 @@ def separate_comma(number)
         end
     end
 
-    return commaized.join
+    commaized.join # implicit return
 end
 
 
@@ -116,6 +116,91 @@ end
 
 =begin
 
+Release 5: Reflect
+In the reflection section of your my_solution file, answer the following 
+questions (make sure everything in this section is commented out in the file).
 
-   
+1. What was your process for breaking the problem down? What different 
+approaches did you consider?
+
+    First, I thought about the general idea of what I want to achieve, then 
+    I think about how I would solve it in real life if I were not using a 
+    computer.  This way, the pseudocode and the code are more of "models" or 
+    "representations" of what would happen in real life if the instructions 
+    were done step-by-step.  
+
+    At first, I thought I can do a for-loop going backwards, since that kind 
+    of for-loop is available in other languages like JavaScript, Perl, C++, 
+    Java, PHP, etc.  
+
+    The for loop is like this (JavaScript Style):
+
+    for (var counter = number_array.length; counter >=0; counter--) {
+        /* code here */
+    }
+
+    or
+
+    for (var counter = number_array.length; counter >=0; counter -= 3) { // goes backwards every 3 instead of every 1
+        /* code here */
+    }
+
+    But I could not find it in Ruby Docs, so I just used a while loop for the 
+    sake of time.  (That's why the pseudocode say "FOR" instead of "WHILE".)
+
+    But aside from that, my general idea was the same.  The code that I used 
+    for the initial solution was very similar to what I would have done if I 
+    were programming in C++ or Java.  
+
+2. Was your pseudocode effective in helping you build a successful initial solution?
+
+    Yes, it helped me because when I thought of the problem, I did not think "How do 
+    I put commas between numbers every 3 digits?" (which is the overall problem).  I 
+    thought of each step, so I already visualized what the individual steps would 
+    look like when I started coding.  
+
+3. What Ruby method(s) did you use when refactoring your solution? What difficulties 
+did you have implementing it/them? Did it/they significantly change the way your code 
+works? If so, how?
+
+    When I refactored the solution, the built-in Ruby methods that were different from 
+    what I used in the initial solution were ".unshift()" and ".pop()".  Which was 
+    disappointing, because I thought that I could get to use something newer or more 
+    different.
+
+    Coding the solution itself was not difficult.  What was difficult was thinking 
+    "Is this refactored enough?  It STILL looks too long."  So I spent a lot of 
+    time looking at Ruby Docs to make sure that there was no other more elegant 
+    alternative to what I did.  
+
+    The refactored solution changed a little bit from the original, because I did 
+    not need to reverse the Array of numbers.  Since ".pop(3)" already allowed me to 
+    get the last 3 elements (which correspond to the last 3 digits), it also changed 
+    what I used to iterate in the while-loop, and I didn't need to put the 
+    if-statement to make sure that the commas only get added ONLY IF the code has 
+    passed 3 digits AND there are no more digits left.  
+
+4. How did you initially iterate through the data structure?
+
+    Initially, I iterated backwards through the data structure (through 
+    .reverse), because I thought that it's how people figure out where to 
+    draw the commas, when they write numbers in real life, if they are not 
+    sure how many digits there are supposed to be (or at least I do it that way).  
+
+    In the alternative initial solution, I tried iterating forwards, but there are now 
+    2 extra lines of code to make it work (although each line is shorter).  So I can't 
+    tell if that alternative solution is better or worse than the original.  
+
+5. Do you feel your refactored solution is more readable than your initial solution? Why?
+
+    I think it is readable to me now, only because I know what those built-in methods 
+    mean.  So the code looks really nice and clean, since there are less lines, less 
+    things to edit, and less points of failure.  
+
+    But if it were my first time learning Ruby and looking at those built-in methods, the 
+    refactored solution might have been not as readable to me because I wouldn't have 
+    understood it.  The longer version would have been more readable just because I can 
+    follow where the logic is going, unlike the built-in methods where the logic is hidden, 
+    and "black-boxed".  
+
 =end
