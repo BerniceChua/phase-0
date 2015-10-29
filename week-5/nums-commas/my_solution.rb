5bd92ce61f669aa46d8cdcdea372e6f545555470
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-28 18:54:05 -0700

    Create week-5/nums-commas directory & 2 .rb files.

---
+++ week-5/nums-commas/my_solution.rb
@@ -0,0 +1,26 @@
+# Numbers to Commas Solo Challenge
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# 0. Pseudocode
+
+# What is the input?
+# What is the output? (i.e. What should the code return?)
+# What are the steps needed to solve the problem?
+
+
+# 1. Initial Solution
+
+
+
+
+# 2. Refactored Solution
+
+
+
+
+# 3. Reflection
