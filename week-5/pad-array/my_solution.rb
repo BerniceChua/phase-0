88ee45338a4d81047699d27f424f04043fac0bc8
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-29 11:49:46 -0700

    Complete the pseudocode.

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -26,13 +26,23 @@ output for pad! = the input array, but modified
 
 for pad
 Step 1: get inputs:
-    1 array
-    1 integer (total # of elements in array)
+    1 array -- called "array"
+    1 integer (total # of elements in array) -- called "min_size"
     OPTIONAL: 1 argument for what we will use to fill out array
         (if not specified, default = nil [the actual value of nil, not a String that says "nil"])
 Step 2:
-
-
+    make new variable to hold results array -- called "copy"
+Step 3:
+    check if length of "array" is smaller than or equal to min_size
+Step 3.1
+    if min_size is smaller than or equal to the lenght of "array", return "copy".
+Step 3.2
+    if "min_size" is larger than length of "array", pad "copy" of array with "value"
+        how?
+        Step 3.2.a:
+            if "value" is NOT specified, put nil as padded elements
+        Step 3.2.b:
+            if "value" IS specified, put value of "value" as padded elements
 
 =end
 
