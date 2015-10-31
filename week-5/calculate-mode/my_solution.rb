3a0929ecd3bc382b5ac9a59028b3d70762a16ebc
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-30 18:51:42 -0700

    Add initial solution.

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -21,24 +21,48 @@
 
 # What are the steps needed to solve the problem?
 Step 1: get input
-Step 2: make variable to hold result -- called "mode"
+Step 2: make variable to hold result -- called "output_mode"
 Step 3: iterate through the input array
 Step 3.1: if the value of this element has NOT yet already been encountered, 
             make it a new key, and add 1 to its counter.
 Step 3.2: if the value of this element has already been encountered,
             add 1 to the counter of its existing key.
-Step 4: return the key with the highest value
+Step 4: choose the key-value pair with the highest number as the value
+Step 5: return the key with the highest value
 
 =end
 
 # 1. Initial Solution
 
+def mode(input_array)
+    output_mode = nil
+    mode_hash = {}
 
+    input_array.each do |variable|
+        if mode_hash.has_key?(variable)
+            mode_hash[variable] += 1
+        else
+            mode_hash[variable] = 1
+        end
+    end
 
+    output_mode = mode_hash.values.sort
+
+    result = mode_hash.select { |k,v| 
+        v == output_mode[-1]
+    }
+
+    return result.keys
+end
 
 # 3. Refactored Solution
 
-
-
+people = {
+  :fred => 23,
+  :joan => 18,
+  :pete => 54
+}
+people.sort_by { |name, age| age }
+  # => [[:joan, 18], [:fred, 23], [:pete, 54]]
 
 # 4. Reflection
