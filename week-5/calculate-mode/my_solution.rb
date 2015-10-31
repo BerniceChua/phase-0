c54c6fa2ff0e46266ce36b5ae092e71057e6e603
BerniceChua <Bernice.Chua.415@gmail.com>
2015-10-30 17:23:51 -0700

    Add pseudocode to calculuate-mode/my_solution.rb.

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -1,6 +1,6 @@
 # Calculate the mode Pairing Challenge
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge [with: Trevor Newcomb (navigator)]
 
 # I spent [] hours on this challenge.
 
@@ -11,11 +11,25 @@
 
 
 # 0. Pseudocode
+=begin
 
 # What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+# an array
 
+# What is the output? (i.e. What should the code return?)
+# the most common element of the array
+
+# What are the steps needed to solve the problem?
+Step 1: get input
+Step 2: make variable to hold result -- called "mode"
+Step 3: iterate through the input array
+Step 3.1: if the value of this element has NOT yet already been encountered, 
+            make it a new key, and add 1 to its counter.
+Step 3.2: if the value of this element has already been encountered,
+            add 1 to the counter of its existing key.
+Step 4: return the key with the highest value
+
+=end
 
 # 1. Initial Solution
 
