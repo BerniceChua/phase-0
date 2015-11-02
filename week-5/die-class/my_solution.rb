7c7b442ae4650083e539e7d2add3770bbd583a1a
Bernice Anne W. Chua <Bernice.Chua.415@gmail.com>
2015-11-02 09:54:33 -0800

    Update my_solution.rb

--- week-5/die-class/my_solution.rb
+++ week-5/die-class/my_solution.rb
@@ -1,42 +1,3 @@
-# Die Class 1: Numeric
-
-# I worked on this challenge [by myself, with: ]
-
-# I spent [] hours on this challenge.
-
-# 0. Pseudocode
-
-# Input:
-# Output:
-# Steps:
-
-
-# 1. Initial Solution
-
-class Die
-  def initialize(sides)
-    # code goes here
-  end
-
-  def sides
-    # code goes here
-  end
-
-  def roll
-    # code goes here
-  end
-end
-
-
-
-# 3. Refactored Solution
-
-
-
-
-
-
-
 # 4. Reflection
 
 =begin
@@ -88,3 +49,45 @@ you have in implementing them?
     as long as the method exists.  
 
 =end
+
+
+
+# Die Class 1: Numeric
+
+# I worked on this challenge [by myself, with: ]
+
+# I spent [] hours on this challenge.
+
+# 0. Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+
+# 1. Initial Solution
+
+class Die
+  def initialize(sides)
+    # code goes here
+  end
+
+  def sides
+    # code goes here
+  end
+
+  def roll
+    # code goes here
+  end
+end
+
+
+
+# 3. Refactored Solution
+
+
+
+
+
+
+
