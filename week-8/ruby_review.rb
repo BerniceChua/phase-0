c1faf66330137835590400ffabed7481c96ecd88
BerniceChua <Bernice.Chua.415@gmail.com>
2015-11-22 23:54:33 -0800

    Create ruby_review.rb.

---
+++ week-8/ruby_review.rb
@@ -0,0 +1,52 @@
+# U2.W6: Testing Assert Statements
+
+# I worked on this challenge [by myself].
+
+
+# 2. Review the simple assert statement
+
+def assert
+  raise "Assertion failed!" unless yield
+end
+
+name = "bettysue"
+assert { name == "bettysue" }
+assert { name == "billybob" }
+
+# 2. Pseudocode what happens when the code above runs
+
+
+
+# 3. Copy your selected challenge here
+
+
+
+
+# 4. Convert your driver test code from that challenge into Assert Statements
+
+
+
+
+
+# 5. Reflection
+
+=begin
+
+Release 2: Reflect
+In the reflection section in your ruby_review.rb file, answer the following questions:
+
+1. What concepts did you review or learn in this challenge?
+
+    I reviewed the concept of assert statements and test-driven development.  
+
+
+2. What is still confusing to you about Ruby?
+
+    fdfd
+
+
+3. What are you going to study to get more prepared for Phase 1?
+
+    fdfs
+
+=end
