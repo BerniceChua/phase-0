9af7d9136665a389946e84184eb1d1c4e4169270
BerniceChua <Bernice.Chua.415@gmail.com>
2015-11-26 15:11:49 -0800

    Copy the code from jsfiddle.

--- week-9/JavaScript/DOM-manipulation/home_page.js
+++ week-9/JavaScript/DOM-manipulation/home_page.js
@@ -1,36 +1,49 @@
 // DOM Manipulation Challenge
 
 
-// I worked on this challenge [by myself, with: ].
+// I worked on this challenge [with: Nil Thacker].
 
 
 // Add your JavaScript calls to this page:
 
-// Release 0:
+// Release 0 aka 1:
+document.getElementById("release-0").className = "done";
+
+
+// Release 1 aka 2:
+document.getElementById("release-1").style.display='none';
+
+
+// Release 2 aka 3:
+document.getElementsByTagName("H1")[0].innerHTML = "I completed release 2.";
+
+
+// Release 3 aka 4:
+document.getElementById("release-3").style.backgroundColor = "#955251";
+
+
+// Release 4 aka 5:
+var elements = document.getElementsByClassName("release-4");
+var size = elements.length;
+
+for (var i = 0; i < size; i++) {
+    var instance = elements[i];
+    instance.style.fontSize = "2em";
+}
+
+
+// Release 5 aka 6:
+var template = document.getElementById("hidden");
+document.body.appendChild(template.content.cloneNode(true));
+
+
+/* Reflection
+// What did you learn about the DOM?
 
 
 
-
-// Release 1:
-
+// What are some useful methods to use to manipulate the DOM?
 
 
 
-// Release 2:
-
-
-
-
-// Release 3:
-
-
-
-
-// Release 4:
-
-
-
-
-// Release 5:
-
-
+*/
